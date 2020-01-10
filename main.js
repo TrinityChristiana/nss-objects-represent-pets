b48bb0d6bb44b35bd4fe683c99a7636ece71af78
Trinity Terry <trinitycterry@gmail.com>
2020-01-10 15:53:35 -0600

    Added play function

--- main.js
+++ main.js
@@ -15,7 +15,14 @@ const pet = {
     },
     sleep:function(){
         console.log("Let me sleep!");
-    }
+    },
+    favoriteToys: [], // Give your pet a new key named favoriteToys whose value is an empty array.
+    play: function (toy){
+        if (toy.includes("fuzzy")){
+            this.favoriteToys.push(toy);
+        }
+    } // Now define another new key named play whose value is a function with a single parameter named toy.
+
 };
 
 // Once you have define the properties, invoke each behavior.
@@ -23,3 +30,8 @@ const pet = {
 pet.bark();
 pet.eat();
 pet.sleep();
+pet.play("fuzzy watch");
+console.log(pet.favoriteToys);
+
+// The function should determine if your pet liked the toy, and if s/he does, then it should be added to the favoriteToys array using the this keyword (see example above).
+// Here's an example. If your pet is a cat, then he likely only likes furry toys, so you could put an if..then condition in the play() method that checks if the word "fuzzy" is in the argument value. If it is, it gets added to the array.
